191445c94059c628809ee212728d2386e7f4686f
Shubham Kumar Bal <mrskb773@gmail.com>
2026-02-08 10:38:34 +0530

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-DATABASE_PATH = os.path.join(BASE_DIR, "database", "rental.db")
-SECRET_KEY = "bike_rental_professional_key"
